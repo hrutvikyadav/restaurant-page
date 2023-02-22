73e6f1b446b2ba74a0f284d3f4096b47f15a2f87
hrutvikyadav <hrutvikyadav@gmail.com>
2023-02-22 15:17:46 +0530

    feat: enabled image bundling using built-in webpack Asset Modules

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './styles.css';
+import Bistro from './bistro-img.jpg';
 
 console.log("Webpack working")
 console.log("working in development mode")
@@ -7,4 +8,9 @@ let contentDiv = document.getElementById('content')
 
 let heading = document.createElement('h1')
 heading.textContent = "Brady's Bistro"
-contentDiv.appendChild(heading) 
+contentDiv.appendChild(heading)
+
+let imageEl = document.createElement('img')
+imageEl.setAttribute('src', Bistro)
+imageEl.setAttribute('id', 'img-element')
+contentDiv.appendChild(imageEl)
